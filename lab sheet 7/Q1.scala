4a982a659cbf984612f7b13754c75f19cf973ed9
Shamalka_M <89902167+Shamalka111@users.noreply.github.com>
2022-09-12 12:31:04 +0530

    Update Q1.scala

--- lab sheet 7/Q1.scala
+++ lab sheet 7/Q1.scala
@@ -1,20 +1,37 @@
     
 case class Rational(numer:Int, denomi:Int) {
 
-    require(denomi>0, println("Denominator must be greater than 0"))   //avoid 0 denominator
+    require(denomi !=0, println("Denominator should not be equal to 0"))   //avoid 0 denominator
 
-    def neg = Rational(-this.numer, this.denomi)
+   def neg() ={
+        
+        if(this.numer>0 && this.denomi<0){
+            Rational(this.numer,-this.denomi)
+
+        }else if(this.numer<0 && this.denomi<0){
+            Rational(this.numer,-this.denomi)
+
+        }else{
+            Rational(-this.numer,this.denomi)
+        }
+        
+    }
+
     
     override def toString(): String = numer + "/" + denomi
 }
 
 object neg extends App {
 
-    val x1 = Rational(1,2)
-    val x2 = Rational(4,5)
+    val x1 = Rational(-1,2)
+    val x2 = Rational(1,-2)
+    val x3 = Rational(1,2)
+    val x4 = Rational(-1,-2)
 
-    println(x1.neg)
-    println(x2.neg)
+    println(x1.neg())
+    println(x2.neg())
+    println(x3.neg())
+    println(x4.neg())
 
 }
 
